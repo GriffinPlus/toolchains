3b5fc70f32c2ba9002855be40aee4b775f79337d
Sascha Falk <sascha@falk-online.eu>
2020-01-06 19:47:54 +0000

    Adjust formatting of cmake toolchain files

--- toolchains/configs/gcc-9.2.0/x86_64-w64-mingw32.toolchain.cmake
+++ toolchains/configs/gcc-9.2.0/x86_64-w64-mingw32.toolchain.cmake
@@ -1,16 +1,15 @@
-set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set( CMAKE_SYSTEM_NAME Windows )
+set( CMAKE_SYSTEM_PROCESSOR x86_64 )
 
-set(CMAKE_SYSROOT /usr/local/x-tools/x86_64-w64-mingw32/x86_64-w64-mingw32/sysroot)
-set(CMAKE_STAGING_PREFIX /tmp/staging)
+set( CMAKE_SYSROOT /usr/local/x-tools/x86_64-w64-mingw32/x86_64-w64-mingw32/sysroot )
+set( CMAKE_STAGING_PREFIX /tmp/staging )
 
-set(tools /usr/local/x-tools/x86_64-w64-mingw32)
-set(CMAKE_C_COMPILER ${tools}/bin/x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER ${tools}/bin/x86_64-w64-mingw32-windres)
+set( tools /usr/local/x-tools/x86_64-w64-mingw32 )
+set( CMAKE_C_COMPILER ${tools}/bin/x86_64-w64-mingw32-gcc )
+set( CMAKE_CXX_COMPILER ${tools}/bin/x86_64-w64-mingw32-g++ )
+set( CMAKE_RC_COMPILER ${tools}/bin/x86_64-w64-mingw32-windres )
 
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
+set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+set( CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY )
