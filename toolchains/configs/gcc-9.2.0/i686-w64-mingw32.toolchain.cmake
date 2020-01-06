90aba8ba8664859884ccc8e0bad721dadc507afe
Sascha Falk <sascha@falk-online.eu>
2020-01-06 20:11:12 +0000

    Add TOOLCHAIN_RUNTIME_FILES variable to cmake toolchain files

--- toolchains/configs/gcc-9.2.0/i686-w64-mingw32.toolchain.cmake
+++ toolchains/configs/gcc-9.2.0/i686-w64-mingw32.toolchain.cmake
@@ -14,3 +14,5 @@ set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
 set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
 set( CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY )
 
+set( TOOLCHAIN_RUNTIME_FILES
+     "/usr/local/x-tools/i686-w64-mingw32/i686-w64-mingw32/sysroot/usr/i686-w64-mingw32/bin/libwinpthread-1.dll" )
